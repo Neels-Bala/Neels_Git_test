b0806b199207bd15c9a6600099e0b9e1cb9c9afd
Neels-Bala <105009735+Neels-Bala@users.noreply.github.com>
2026-03-22 14:28:26 +0530

    Testing file can be opened in VS

---
+++ Neels_Learn.py
@@ -0,0 +1,9 @@
+recent_searches = ["Python Tutorial", "How to build AI", "Best Pizza In town"]
+print(recent_searches)
+agent_config = {
+    "model_name" : "Latest_model",
+    "max_tokens" : 250,
+    "temparature" : 0.7
+
+}
+print(agent_config["model_name"])
